12afd998ba06d283e2aaeb927910d907a83c7983
Juho <juhoannala3@gmail.com>
2025-10-07 22:35:02 +0300

    luokka luotu

--- Versiohallinta/main.py
+++ Versiohallinta/main.py
@@ -2,5 +2,14 @@ def funktio(a, b):
     return a + b
 
 
+class Luokka:
+    def __init__(self, kirjain):
+        self.__kirjain = kirjain
+    
+    def nayta_kirjain(self):
+        print(self.__kirjain)
+
+
+        
 if __name__ == "__main__":
     print(funktio(3, 8))
